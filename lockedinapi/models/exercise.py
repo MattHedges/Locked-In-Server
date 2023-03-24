9925672b274011480dcc206df288956e738613fa
Matthew Hedges <matthedges35@gmail.com>
2023-03-23 19:03:01 -0500

    all done

--- lockedinapi/models/exercise.py
+++ lockedinapi/models/exercise.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 class Exercise(models.Model):
 
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=1000)
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=1000, null=False)
     difficulty = models.ForeignKey("Difficulty", on_delete=models.CASCADE)
     muscleGroup = models.ForeignKey("MuscleGroup", on_delete=models.CASCADE)
     equipment = models.ForeignKey("Equipment", on_delete=models.CASCADE)
-    video = models.CharField(max_length=200)
+    video = models.CharField(max_length=200, null=True)
--- lockedinapi/models/routine.py
+++ lockedinapi/models/routine.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 class Routine(models.Model):
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     exerciseRoutine = models.ManyToManyField("exercise", through="ExerciseRoutine", related_name= 'routine' )
